004e887d1eb1a616119ab75e286b4b8e796fd094
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-03-16 14:21:14 -0400

    Initial Commit

---
+++ SchoolPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchoolPlanner
+//
+//  Created by Diego Sierraalta on 2021-03-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SchoolPlanner/SchoolPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolPlannerApp.swift
+//  SchoolPlanner
+//
+//  Created by Diego Sierraalta on 2021-03-16.
+//
+
+import SwiftUI
+
+@main
+struct SchoolPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
