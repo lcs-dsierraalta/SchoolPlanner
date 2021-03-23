6ad5a36b8a31b9675ed3e0b090ceea017e77ce35
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-03-22 20:17:34 -0400

    Added a new type called Assignment priority, which will be used to determine the priority of an assignment

---
+++ SchoolPlanner/Model/AssignmentPriority.swift
@@ -0,0 +1,15 @@
+//
+//  AssignmentPriority.swift
+//  SchoolPlanner
+//
+//  Created by Diego Sierraalta on 2021-03-22.
+//
+
+import Foundation
+
+
+enum AssignmentPriority {
+    case high
+    case medium
+    case low
+}
--- SchoolPlanner/Model/Assignments.swift
+++ SchoolPlanner/Model/Assignments.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+class Assignment: Identifiable {
+    
+    var id = UUID()
+    var assignmentName: String
+    var description: String
+    var completed: Bool
+    var priority: AssignmentPriority
+    var dueDate = Date()
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+    
+}
